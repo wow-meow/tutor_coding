a79b44c7f123401c89b9d0c0f6aa0bc49a70dfe4
Wow Meow <wowmeow@duck.com>
2024-10-31 18:48:12 +0800

    modified C project "thread_func_multi_args_in_out"

--- tutor_c_cpp/multithreading/mt_c/thread_func_multi_args_in_out/main.c
+++ tutor_c_cpp/multithreading/mt_c/thread_func_multi_args_in_out/main.c
@@ -15,30 +15,68 @@ typedef struct myIntegerVector {
 } intVec_t;
 
 void *createIntVector(void *paramPtr);
-void *consumeIntVector(void *intVecPtr);
+void *printIntVector(void *intVecPtr);
 
 int main(void)
 {
-    printf("== Showcase when pthread methods fail ==\n\n");
+    //printf("== Showcase when pthread methods fail ==\n\n");
+    printf("== Pass in and return multiple arguments to / from a thread function ==\n\n");
 
     pthread_attr_t th_attr;
     pthread_attr_init(&th_attr);
     pthread_attr_setschedpolicy(&th_attr, SCHED_FIFO);
 
-    //pthread_t tid1;
-    //if (pthread_create(&tid1, (pthread_attr_t *)0x1234, NULL, NULL) != 0) {
-    //    perror("pthread_create");
-    //    return 1;
-    //}
+    pthread_t tid1;
+    if (pthread_create(&tid1, (pthread_attr_t *)0xffffffff, NULL, NULL) != 0) {
+        perror("pthread_create");
+        return 1;
+    }
 
     const int N_threads = 5;
-    pthread_t tids_producer[N_threads];
-    pthread_t tids_consumer[N_threads];
+    pthread_t tids[N_threads];
     paramIntVec_t param[N_threads];
+
+    // producers create vectors
     for (int i = 0; i < N_threads; ++i) {
         param[i].th_index = i;
         param[i].len = 2 * i;
-        if (pthread_create(&tids_producer[i], &th_attr, createIntVector, &param[i]) != 0) {
+        if (pthread_create(&tids[i], &th_attr, createIntVector, &param[i]) != 0) {
+            perror("pthread_create");
+            return 1;
+        }
+    }
+
+    /**
+     * pthread_join() will not fix the running order of threads
+     * */
+
+    void *resPtrs[N_threads];
+    for (int i = 0; i < N_threads; ++i) {
+        if (pthread_join(tids[i], &resPtrs[i]) != 0) {
+            perror("pthread_join");
+            return 1;
+        }
+
+        // print what threads have created
+        intVec_t *pIntVec = (intVec_t *) resPtrs[i];
+        printf("> Thread %d creates:\n", pIntVec->th_index);
+        for (int i = 0; i < pIntVec->len; ++i) {
+            printf("[%d] %d, ", i, pIntVec->data[i]);
+        }
+        putchar('\n');
+
+        // dealloc heap memory if they are not needed afterwards
+        //free(pIntVec->data);
+        //free(pIntVec);
+    }
+
+    printf("\n... The Creation of Integer Vectors Accomplished! "
+           "Start to use multiple threads to print out vectors.\n");
+
+    // consumers print out vectors
+    for (int i = 0; i < N_threads; ++i) {
+        if (pthread_create(&tids[i], &th_attr, printIntVector,
+            resPtrs[N_threads - 1 - i]) != 0) {
             perror("pthread_create");
             return 1;
         }
@@ -46,10 +84,19 @@ int main(void)
 
     for (int i = 0; i < N_threads; ++i) {
         void *res;
-        if (pthread_join(tids_producer[i], &res) != 0) {
+        if (pthread_join(tids[i], &res) != 0) {
             perror("pthread_join");
             return 1;
         }
+        int th_index = *((int *) res);
+        printf(">>> [Thread %d] finishes printing the vector created by Thread %d\n", i, th_index);
+        free(res);
+    }
+
+    // de-allocate heap memory
+    for (int i = 0; i < N_threads; ++i) {
+        free(((intVec_t *)resPtrs[i])->data);
+        free(resPtrs[i]);
     }
 
     pthread_exit(NULL);
@@ -58,39 +105,42 @@ int main(void)
 
 void *createIntVector(void *paramPtr) {
     paramIntVec_t param = *((paramIntVec_t *)paramPtr);
-    intVec_t *res = malloc(sizeof(intVec_t));
+    intVec_t *pIntVec = malloc(sizeof(intVec_t));
     int th_index = param.th_index;
     int len = param.len;
 
     printf("> [Thread %d] is CREATING the integer vector.\n", th_index);
-    res->th_index = th_index;
-    res->len = len;
+    pIntVec->th_index = th_index;
+    pIntVec->len = len;
     if (len > 0) {
-        res->data = malloc(sizeof(int) * len);
+        pIntVec->data = malloc(sizeof(int) * len);
         for (int i = 0; i < len; ++i) {
-            res->data[i] = 10 * th_index;
+            pIntVec->data[i] = 10 * th_index;
         }
     } else {
-        res->data = NULL;
+        pIntVec->data = NULL;
     }
-    printf("--> [Thread %d] vector data stored at 0x%p.\n", th_index, res->data);
-    pthread_exit(res);
+    printf("--> [Thread %d] vector data stored at 0x%p.\n", th_index, pIntVec->data);
+    pthread_exit(pIntVec);
     //return NULL;
 }
 
-void *consumeIntVector(void *intVecPtr) {
-    // print what threads created
+// print what threads created
+void *printIntVector(void *intVecPtr) {
     intVec_t *pIntVec = (intVec_t *) intVecPtr;
-    printf("> Thread %d creates:\n", pIntVec->th_index);
+    printf("\n--> printIntVector(): [Thread %d] has created:\n", pIntVec->th_index);
     for (int i = 0; i < pIntVec->len; ++i) {
         printf("[%d] %d, ", i, pIntVec->data[i]);
     }
-    putchar('\n');
+    printf("\n\n");
 
-    free(pIntVec->data);
-    free(pIntVec);
+    // dealloc heap memory if they are not needed afterwards
+    //free(pIntVec->data);
+    //free(pIntVec);
 
-    pthread_exit(NULL);
+    // configure what to return
+    int *p_th_index = malloc(sizeof(int));
+    *p_th_index = pIntVec->th_index;
+    pthread_exit(p_th_index);
     //return NULL;
 }
-
