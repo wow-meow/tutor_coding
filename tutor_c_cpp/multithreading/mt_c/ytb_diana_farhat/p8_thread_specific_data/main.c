7d07ce684198108aad9f61ab7290d1ac51dec0ef
Wow Meow <wowmeow@duck.com>
2024-11-03 17:22:45 +0800

    created a C project "p8_thread_specific_data"

---
+++ tutor_c_cpp/multithreading/mt_c/ytb_diana_farhat/p8_thread_specific_data/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h> // malloc, free
+#include <pthread.h> // POSIX Thread Library
+#include <unistd.h> // usleep, sleep
+#include <string.h> // strlen, strcpy
+#include <time.h>
+
+//__thread int counter = 0;
+//thread_local int counter = 0;
+
+int main(void)
+{
+    printf("== Thread-Specific Data ==\n\n");
+    return 0;
+}
