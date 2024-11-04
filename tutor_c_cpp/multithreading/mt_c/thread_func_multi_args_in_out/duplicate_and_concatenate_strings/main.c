3ddd7790716019f6754e95fc88d7e273fd442120
Wow Meow <wowmeow@duck.com>
2024-11-04 08:53:59 +0800

    updated C project "duplicate_and_concatenate_strings"

--- tutor_c_cpp/multithreading/mt_c/thread_func_multi_args_in_out/duplicate_and_concatenate_strings/main.c
+++ tutor_c_cpp/multithreading/mt_c/thread_func_multi_args_in_out/duplicate_and_concatenate_strings/main.c
@@ -5,138 +5,86 @@
 #include <string.h> // strlen, strcpy
 #include <time.h>
 
-//__thread int counter = 0;
-//thread_local int counter = 0;
-
 typedef struct MyString {
     int len;
     char *data;
 } string_t;
 
-void *sayHelloToSomeone_cstring(void *arg);
-void *sayHelloToSomeone(void *arg);
-
 int duplicateAndConcatenate(char **res, int *ptr_len_res,
     const char src[], const char separator[], int n_copies);
 
+typedef void *(*thread_func_t)(void *);
+void *sayHelloToSomeone_cstring_thfunc(void *arg);
+void *sayHelloToSomeone_string_t_thfunc(void *arg);
+
+int create_join_thread_cstring(void *rets[], int n_threads, thread_func_t thread_func,
+    const char src[], const char separator[]);
+int create_join_thread_string_t(void *rets[], int n_threads, thread_func_t thread_func,
+    const char src[], const char separator[]);
+
+
 int main(void)
 {
-    printf("== Duplicate and Concatenate Strings - Yet Another Example ==\n\n");
+    printf("== Duplicate and Concatenate Strings\n -- Yet Another Example ==\n\n");
 
-    const int N_threads = 2;
-    pthread_t tids[N_threads] = {};
+    printf("\n=== {char *} ===\n\n");
 
-    pthread_attr_t th_attr;
-    pthread_attr_init(&th_attr);
-    pthread_attr_setschedpolicy(&th_attr, SCHED_FIFO);
+    const int n_threads = 2;
+    void *rets[n_threads] = {};
+    int err = 0;
 
-    /**
-     * pthread_create
-     **/
-
-    // cstring, char array
-    char* names_cstring[N_threads] = {}; // an array of names (string, char array)
-    int nameLengths[N_threads] = {};
-    for (int i = 0; i < N_threads; ++i) {
-        int errnum = duplicateAndConcatenate(&names_cstring[i], &nameLengths[i],
-            "Tom", " - ", 1 + i);
-        if (errnum != 0) { return 1; }
-
-        errnum = pthread_create(
-            &tids[i], &th_attr, sayHelloToSomeone_cstring, &names_cstring[i]);
-        free(names_cstring[i]);
-
-        if (errnum != 0) {
-            perror("pthread_create");
-            fprintf(stderr, "errcode %d: %s\n", errnum, strerror(errnum));
-            return 1;
-        }
-    }
-
-    /**
-     * pthread_join
-     * */
-
-    //char *ret_cstrings[N_threads] = {};
-    void *ret_cstrings[N_threads] = {};
-    for (int i = 0; i < N_threads; ++i) {
-        int errnum = pthread_join(tids[i], &ret_cstrings[i]);
-        if (errnum != 0) {
-            perror("pthread_create");
-            fprintf(stderr, "errcode %d: %s\n", errnum, strerror(errnum));
-            return 1;
-        }
-    }
+    err = create_join_thread_cstring(rets, n_threads, sayHelloToSomeone_cstring_thfunc, "Jerry", ".");
+    printf("err: %d\n", err);
 
     /**
      * print out results
      **/
 
-    for (int i = 0; i < N_threads; ++i) {
-        char *res = (char *) ret_cstrings[i];
+    for (int i = 0; i < n_threads; ++i) {
+        char *res = (char *) rets[i];
         printf("Thread %d said: %s\n", i, res);
+        free(res);
     }
 
-    /**
-     * pthread_create
-     **/
+    printf("\n=== {string_t} ===\n\n");
 
-    // struct MyString, string_t
-    string_t names[N_threads] = {}; // an array of names (string, char array)
-    for (int i = 0; i < N_threads; ++i) {
-        int err = 0;
-
-        err = duplicateAndConcatenate(&names[i].data, &names[i].len,
-            "Tom", " - ", 1 + i);
-        if (err != 0) { return 1; }
-
-        err = pthread_create(&tids[i], &th_attr, sayHelloToSomeone, &names[i]);
-        if (err != 0) {
-            perror("pthread_create");
-            fprintf(stderr, "errcode %d: %s\n", err, strerror(err));
-            return 1;
-        }
-
-        //free(names[i].data);
-
-    }
-
-    /**
-     * pthread_join
-     * */
-
-    void *ret_strings[N_threads] = {};
-    for (int i = 0; i < N_threads; ++i) {
-        int err = pthread_join(tids[i], &ret_strings[i]);
-        if (err != 0) {
-            perror("pthread_create");
-            fprintf(stderr, "errcode %d: %s\n", err, strerror(err));
-            return 1;
-        }
-    }
+    err = create_join_thread_string_t(rets, n_threads, sayHelloToSomeone_string_t_thfunc, "Tom", "-");
+    printf("err: %d\n", err);
 
     /**
      * print out results
      **/
 
-    for (int i = 0; i < N_threads; ++i) {
-        string_t *res = (string_t *) ret_strings[i];
+    for (int i = 0; i < n_threads; ++i) {
+        string_t *res = (string_t *) rets[i];
         printf("Thread %d said: %s\n", i, res->data);
         free(res->data);
+        free(res);
     }
 
     pthread_exit(NULL);
     return 0;
 }
 
-void *sayHelloToSomeone_cstring(void *arg)
+/**
+ * 
+ * @param arg {void *} is passed in an address of {char *} c-string, so `arg` is a {char **}
+ * @return 
+ */
+void *sayHelloToSomeone_cstring_thfunc(void *arg)
 {
-    char *name = (char *) arg;
+    /**
+     * arg is in fact a double pointer to char {char **}
+     * */
+
+    //char *name = (char *) arg; // <-- no error thrown out, the result is erroneous
+    char *name = *((char **) arg);
     printf("Hello, (name) %s!\n", name);
 
     char str_part1[] = "Hello, ";
-    char str_part2[] = "!\n";
+    char str_part2[] = "!";
     int len_whole_str = strlen(str_part1) + strlen(name) + strlen(str_part2);
+
     char *res = (char *) malloc(sizeof(char) * (1 + len_whole_str));
     strcpy(res, str_part1);
     strcat(res, name);
@@ -147,7 +95,7 @@ void *sayHelloToSomeone_cstring(void *arg)
     //return NULL;
 }
 
-void *sayHelloToSomeone(void *arg)
+void *sayHelloToSomeone_string_t_thfunc(void *arg)
 {
     string_t *name = (string_t *) arg;
     char str_part1[] = "Hello, ";
@@ -169,7 +117,7 @@ void *sayHelloToSomeone(void *arg)
 int duplicateAndConcatenate(char **res, int *ptr_len_res,
     const char src[], const char separator[], int n_copies)
 {
-    printf(">>> in duplicateAndConcatenate():\n");
+    //printf(">>> in duplicateAndConcatenate():\n");
 
     int len = strlen(src) * n_copies + strlen(separator) * (n_copies - 1);
     *ptr_len_res = len;
@@ -196,6 +144,107 @@ int duplicateAndConcatenate(char **res, int *ptr_len_res,
                 fprintf(stderr, "ERROR: strcat(%s, %s)\n", *res, src);
                 return 1;
             }
+        }
+    }
+
+    return 0;
+}
+
+int create_join_thread_string_t(void *rets[], int n_threads, thread_func_t thread_func,
+    const char src[], const char separator[])
+{
+    pthread_t tids[n_threads] = {};
+
+    pthread_attr_t th_attr;
+    pthread_attr_init(&th_attr);
+    pthread_attr_setschedpolicy(&th_attr, SCHED_FIFO);
+
+    /**
+     * - exit_status defined in a pointer to string_t
+     * - It must be a pointer, because we need to allocate heap memory instead of using stack memory.
+     * - It points to a string_t variable that is what a thread creates.
+     *
+     * pthread_create
+     **/
+
+    // struct MyString, string_t
+    string_t whole_strs[n_threads] = {}; // an array of names (string, char array)
+    for (int i = 0; i < n_threads; ++i) {
+        printf("[Main Thread] duplicateAndConcatenate()\n");
+        int err = duplicateAndConcatenate(&whole_strs[i].data, &whole_strs[i].len,
+            src, separator, 1 + i);
+        if (err != 0) { return 1; }
+
+        err = pthread_create(&tids[i], &th_attr, thread_func, &whole_strs[i]);
+        if (err != 0) {
+            perror("pthread_create");
+            fprintf(stderr, "errcode %d: %s\n", err, strerror(err));
+            return 1;
+        }
+        //free(names[i].data);
+    }
+
+    /**
+     * pthread_join
+     * */
+
+    for (int i = 0; i < n_threads; ++i) {
+        int err = pthread_join(tids[i], &rets[i]);
+        if (err != 0) {
+            perror("pthread_create");
+            fprintf(stderr, "errcode %d: %s\n", err, strerror(err));
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int create_join_thread_cstring(void *rets[], int n_threads, thread_func_t thread_func,
+    const char src[], const char separator[])
+{
+    pthread_t tids[n_threads] = {};
+
+    pthread_attr_t th_attr;
+    pthread_attr_init(&th_attr);
+    pthread_attr_setschedpolicy(&th_attr, SCHED_FIFO);
+
+    /**
+     * - exit_status defined in a pointer to c-string {char *}
+     * - the c-string is exactly what a thread creates
+     * - {char *} is already legal to allocate heap memory to pass out exit_status,
+     * - no need to define a double pointer {char **}
+     *
+     * pthread_create
+     **/
+
+    char* whole_strs[n_threads] = {}; // an array of names (string, char array)
+    int nameLengths[n_threads] = {};
+    for (int i = 0; i < n_threads; ++i) {
+        printf("[Main Thread] duplicateAndConcatenate()\n");
+        int err = duplicateAndConcatenate(&whole_strs[i], &nameLengths[i],
+            src, separator, 1 + i);
+        if (err != 0) { return 1; }
+
+        err = pthread_create(&tids[i], &th_attr, thread_func, &whole_strs[i]);
+        if (err != 0) {
+            perror("pthread_create");
+            fprintf(stderr, "errcode %d: %s\n", err, strerror(err));
+            return 1;
+        }
+        //free(names_cstring[i]);
+    }
+
+    /**
+     * pthread_join
+     * */
+
+    for (int i = 0; i < n_threads; ++i) {
+        int err = pthread_join(tids[i], &rets[i]);
+        if (err != 0) {
+            perror("pthread_create");
+            fprintf(stderr, "errcode %d: %s\n", err, strerror(err));
+            return 1;
         }
     }
 
