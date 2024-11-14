6f9e5e5d3a22f4ddfa0452c73c883dc1753576cc
Wow Meow <wowmeow@duck.com>
2024-11-14 16:41:57 +0800

    updated embed proj1 main.cpp

--- embed/platformio/board_arduino_due/arduino/proj1/src/main.cpp
+++ embed/platformio/board_arduino_due/arduino/proj1/src/main.cpp
@@ -1,8 +1,37 @@
 #include <Arduino.h>
+
+/**
+ * Setup function for Arduino.
+ * This function is called once when the device starts.
+ * It is used to initialize pins, set up communication, etc.
+ */
 void setup() {
-// write your initialization code here
+    // Example: blink the built-in LED
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    // Example: print a message to the serial monitor
+    Serial.begin(9600);
+    Serial.println("Hello, Arduino!");
+
+    // Example: blink the LED every second
+    unsigned long previousMillis = 0;
+    const long intervalMillis = 1000; // blink every 1 second
+
+    while (true) {
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= intervalMillis) {
+            previousMillis = currentMillis;
+            digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+        }
+    }
+
 }
 
+/**
+ * Main loop function for Arduino.
+ * This function is called repeatedly after setup() is completed.
+ * It is used to implement the main logic of the program.
+ */
 void loop() {
 // write your code here
 }
