b79916f73b9ce315ae9ed5da4a4ef0632ad5c7fd
Wow Meow <wowmeow@duck.com>
2024-11-16 18:27:20 +0800

    updated C project "function_pointer"

--- tutor_c_cpp/function_pointer/main.c
+++ tutor_c_cpp/function_pointer/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include <assert.h>
 
 void print_gfg(void) {
     printf("Geeks-for-Geeks\n");
@@ -8,54 +7,9 @@ void print_gfg(void) {
 
 int multiply(const int a, const int b) { return a * b; }
 
-/**
- * Test multiply function
- */
-void test_multiply() {
-    // Test case 1: Positive numbers
-    assert(multiply(2, 3) == 6);
-    printf("multiply(2, 3) = %d\n-> PASSED\n", multiply(2, 3));
-
-    // Test case 2: Zero
-    assert(multiply(0, 5) == 0);
-    printf("multiply(0, 5) = %d\n-> PASSED\n", multiply(0, 5));
-
-    // Test case 3: Negative numbers
-    assert(multiply(-2, 4) == -8);
-    printf("multiply(-2, 4) = %d\n-> PASSED\n", multiply(-2, 4));
-}
-
-/**
- * Print the return value of a function
- * @param funcptr
- * @param x
- * @param y
- */
 void print_retval(int (*funcptr)(int, int), int x, int y) {
     puts("\nprint_retval(int (*funcptr)(int, int), int x, int y)");
     printf("retval: %d\n", funcptr(x, y));
-}
-
-// Mock function for testing
-int mock_func(int x, int y) { return x + y; }
-
-/**
- * Test function for print_retval
- */
-void test_print_retval()
-{
-    int x = 5;
-    int y = 3;
-    print_retval(mock_func, x, y);
-    // Assert the expected output
-    assert(mock_func(x, y) == 8);
-}
-
-void print_one_int(const char *str, int val);
-
-void print_one_int(const char *str, int val) {
-    printf("\nprint_one_int(const char *str, int val)\n");
-    printf(str, val);
 }
 
 void print_one_float(const char *str, float val) {
@@ -92,14 +46,7 @@ void print_cell(char* s, cell_t c) {
 typedef void (*func_printcell_t)(char *, cell_t);
 typedef void (*func_print_variadic_t)(char *, ...);
 
-int main(int argc, char *argv[])
-{
-    test_multiply();
-
-    return 0;
-
-    test_print_retval();
-
+int main(int argc, char *argv[]) {
     printf("\nYou've entered %d arguments.\n", argc);
     for (int i = 0; i < argc; ++i) {
         printf("[%d] %s\n", i, argv[i]);
@@ -137,8 +84,8 @@ int main(int argc, char *argv[])
 
     typedef void (*funcptr_variadic_t)(const char *s, ...);
     funcptr_variadic_t funcptr = (funcptr_variadic_t) print_one_float;
-    float fl_val = 0.5f;
-    funcptr("%.2f\n", (double) fl_val);
+    float fl_val = 0.5;
+    funcptr("%.2f\n", fl_val);
     funcptr = (funcptr_variadic_t) print_one_double;
     double db_val = 0.5;
     funcptr("%.2f\n", db_val);
