b68bd27a4dbd6b06574c165b96b30c09772bce4a
Wow Meow <wowmeow@duck.com>
2024-11-19 12:55:39 +0800

    updated C project 'func_ptr'

--- tutor_c_cpp/multithreading/mt_c/ytb_jacob_sorber/func_ptr/src/main.c
+++ tutor_c_cpp/multithreading/mt_c/ytb_jacob_sorber/func_ptr/src/main.c
@@ -10,6 +10,30 @@
  * 'op' is a function pointer parameter that represents an operation
  * It takes an int and two doubles as input and returns an int 
  * This allows for flexible use of different operations within the 'foo' function
+ * 
+ * @param op A function pointer to the operation to be performed.
+ *           The function should take two int parameters and return an int.
+ * @param a The first integer operand.
+ * @param b The second integer operand.
+ * @return The result of the operation applied to a and b.  
+ * 
+ * @example
+ * int (*foo) (int (*op) (int, double, double), int, int);
+ * int result = foo(someOperation, 5, 10);
+ * 
+ * @note
+ * The function pointer type 'int (*)(int, double, double)' 
+ * matches the type of the function 'someOperation'
+ * 
+ * indentifier: foo
+ * right: nothing
+ * left: pointer!
+ * pop out the paranthesis
+ * right: function pointer type 'int (*)(int, double, double)' and takes two int parameters
+ * left: returns int pointer
+ *
+ * Therefore, foo is a pointer to a function that takes a function pointer and two int parameters and 
+ * returns a pointer to an int
  */ 
 int *(*foo) (int (*op) (int, double, double), int, int);
 
