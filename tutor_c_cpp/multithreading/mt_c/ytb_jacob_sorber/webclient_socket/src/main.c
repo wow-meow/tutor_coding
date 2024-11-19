a259e5ada45907c04e4a0d92bb4bdd1a5819e839
Wow Meow <wowmeow@duck.com>
2024-11-19 12:23:56 +0800

    created C project 'webclient_socket' based on YTB Jacob Sorber's tutorial

---
+++ tutor_c_cpp/multithreading/mt_c/ytb_jacob_sorber/webclient_socket/src/main.c
@@ -0,0 +1,14 @@
+#include "socket.h"
+
+int main(int argc, char **argv) 
+{
+    printf("== Build a web client, socket ==\n\n");
+    
+    int sockfd, n;
+    int sendbytes;
+    struct sockaddr_in servaddr;
+    char sendline[MAXLINE];
+    char recvline[MAXLINE];
+
+    return 0;
+}
---
+++ tutor_c_cpp/multithreading/mt_c/ytb_jacob_sorber/webclient_socket/src/socket.h
@@ -0,0 +1,37 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> // sleep
+
+#include <arpa/inet.h>
+
+#include <stdarg.h> // variadic func
+#include <errno.h>
+#include <fcnctl.h>
+
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+
+/**
+ * Preprocessor macros for readability
+ */
+
+// standard HTTP port is 80
+#define SERVER_PORT 80
+// the len of the buffer where data goes
+#define MAXLINE 4096
+// less wordy types
+// ?? why not typedef
+#define SA struct sockaddr
+
+void err_n_die(const char *fmt, ...);
+
+#endif // SOCKET_H
