5f83670ca03ab60c6d95ab09e23b77b50776b870
Wow Meow <wowmeow@duck.com>
2024-11-20 22:01:42 +0800

    recreated Cpp project 'helloworld'

--- tutor_c_cpp/helloworld/helloworld.cpp
+++ tutor_c_cpp/helloworld/helloworld.cpp
@@ -2,18 +2,18 @@
 #include <vector>
 #include <string>
 
-//using namespace std;
-using std::vector;
-using std::cout;
-using std::endl;
-using std::string;
+using namespace std;
 
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    cout << endl;
     for (const string& word : msg)
     {
         cout << word << " ";
     }
     cout << endl;
+
+    cout << "\nHello, World!!!\n";
 }
