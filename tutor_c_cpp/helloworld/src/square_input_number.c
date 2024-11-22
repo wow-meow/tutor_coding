757a866e40e0f8d722b76e7d15efad49ffe8a3cc
Wow Meow <wowmeow@duck.com>
2024-11-22 11:02:40 +0800

    organized c/cpp project 'helloworld', added a Makefile

--- tutor_c_cpp/helloworld/src/square_input_number.c
+++ tutor_c_cpp/helloworld/src/square_input_number.c
@@ -6,38 +6,34 @@
 #include <stdio.h>
 
 // Generic function to handle both integer and floating-point numbers
-#define process_number(x) _Generic((x), \
-    int: process_int, \
-    double: process_double \
+#define square_number(x) _Generic((x), \
+    int: square_int, \
+    double: square_double \
 )(x)
 
 // Type-specific processing functions
-static int process_int(int x) {
-    return x * x;
-}
+static int square_int(int x) { return x * x; }
 
-static double process_double(double x) {
-    return x * x;
-}
+static double square_double(double x) { return x * x; }
 
 int main()
 {
-    int num_int;
-    double num_double;
+    int i_num;
+    double d_num;
 
     printf("Processing different numeric types:\n\n");
     
     // Handle integer input
     printf("1. Integer input:\n");
     printf("Enter an integer: ");
-    scanf("%d", &num_int);
-    printf("The square of %d is %d\n\n", num_int, process_number(num_int));
+    scanf("%d", &i_num);
+    printf("The square of %d is %d\n\n", i_num, square_number(i_num));
     
     // Handle floating-point input
     printf("2. Floating-point input:\n");
     printf("Enter a number: ");
-    scanf("%lf", &num_double);
-    printf("The square of %.2f is %.2f\n", num_double, process_number(num_double));
+    scanf("%lf", &d_num);
+    printf("The square of %.2f is %.2f\n", d_num, square_number(d_num));
     
     return 0;
 }
