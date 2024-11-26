02c97083bd2881319d2d74d28ede8f248981fa39
Wow Meow <wowmeow@duck.com>
2024-11-26 09:53:28 +0800

    update datacamp intermediate python

--- tutor_python/datacamp/intermediate_python/ch03_01_logic.py
+++ tutor_python/datacamp/intermediate_python/ch03_01_logic.py
@@ -3,9 +3,9 @@
 import numpy as np
 import pandas as pd
 
-val = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
-weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
-bmi = weight / val ** 2
+heights = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
+weights = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
+bmi = weights / heights ** 2
 print(bmi)
 
 print(f"2 > 3 : {2 > 3}")
